926a801a380904c4f05951d5a2ac35864b6c51df
Vincent Yu <vantoriarin@gmail.com>
2018-02-02 02:08:09 +0900

    Added conversaiont UI script.

---
+++ New Unity Project/Assets/Script/DialogueManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    private string sentences;
+    bool DisplayNext = false;
+    int SentencesCount;
+
+	// Use this for initialization
+	void Start () {
+        int SentencesCount = 0;
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log(dialogue.name +" : "+ dialogue.sentences[SentencesCount]);
+        if (DisplayNext)
+        {
+            Debug.Log(dialogue.name + " : " + dialogue.sentences[SentencesCount]);
+        }
+    }
+
+    public void DisplayNextSentences()
+    {
+        DisplayNext = !DisplayNext;
+        SentencesCount += 1;
+        Debug.Log("Display Next" + DisplayNext);
+    }
+    void EndDialogue()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log(SentencesCount);
+	}
+}
